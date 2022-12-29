d9926e1ef652d9a5f218b8a015a6ff1735a38f46
Rmcstt <anisio.nanato@hotmail.com>
2022-12-29 08:40:17 -0300

    organization

---
+++ 2-oc/bad/main.py
@@ -0,0 +1,12 @@
+from solution.client import GithubClient
+from repo.parse import RepoParser
+
+
+if __name__ == "__main__":
+  username = 'Rmcstt'
+  response = GithubClient.get_repos_by_user(username)
+
+  if response["status_code"] == 200:
+    RepoParser.parse(response["body"])
+  else:
+    print(response["body"])
