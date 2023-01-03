11f0b8eeb0d0cb706c87da44724a7fa0b0653c67
Rmcstt <anisio.nanato@hotmail.com>
2023-01-03 08:44:14 -0300

    add god version of openclose principle

--- 2-oc/god/main.py
+++ 2-oc/god/main.py
@@ -1,6 +1,8 @@
 from github.client import GithubClient
 from repo.parse import RepoParser
 from repo.reports_generator import ReportsGenerator
+from repo.reports.html_generator import HTMLGenerator
+from repo.reports.markdown_generator import MarkdownGenerator
 
 
 if __name__ == "__main__":
@@ -9,8 +11,8 @@ if __name__ == "__main__":
 
   if response["status_code"] == 200:
     repos = RepoParser.parse(response["body"])
-    markdown_report = ReportsGenerator.build("MARKDOWN", repos)
-    html_report = ReportsGenerator.build("HTML", repos)
+    markdown_report = ReportsGenerator.build(MarkdownGenerator, repos)
+    html_report = ReportsGenerator.build(HTMLGenerator, repos)
 
     print(markdown_report)
     print(html_report)
